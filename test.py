bb649e622aa5c7880bc98f655fd09e427cc56ba2
ankoman <i08320.wctsonhb@gmail.com>
2024-02-02 01:00:53 +0900

    1 round encryptio test pass

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
-from aes import AES
+from aes import AES, text2matrix, matrix2text
 
 def main():
     master_key = 0x000102030405060708090a0b0c0d0e0f
     aes = AES(master_key)
 
-    plaintext = 0x00112233445566778899aabbccddeeff
-    encrypted = aes.encrypt(plaintext)
-    print(f"{encrypted:032x}")
+    ival_before_9thMC = 0x00112233445566778899aabbccddeeff
+    aes.state_matrix = text2matrix(ival_before_9thMC)
+    print(aes.state_matrix)
 
-    decrypted = aes.decrypt(encrypted)
-    print(f"{decrypted:032x}")
+    aes._AES__add_round_key(aes.state_matrix, aes.round_keys[:4])
+
+    aes._AES__sub_bytes(aes.state_matrix)
+    aes._AES__shift_rows(aes.state_matrix)
+    aes._AES__mix_columns(aes.state_matrix)
+    aes._AES__add_round_key(aes.state_matrix, aes.round_keys[4 * 1 : 4 * (1 + 1)])
+
+    print(f"{matrix2text(aes.state_matrix):x}")
+
+    # Correct ctxt = round[ 2].start 89d810e8855ace682d1843d8cb128fe4
 
 if __name__ == '__main__':
     main()
