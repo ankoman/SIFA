f1ff66f867d0746af99963dd1d41bf4ce575e3d2
ankoman <i08320.wctsonhb@gmail.com>
2024-05-19 15:57:10 +0900

    Add base transform matrix

--- masking/masked_aes.py
+++ masking/masked_aes.py
@@ -14,7 +14,10 @@ class GF2:
 
     def __repr__(self):
         return str(self.val)
-    
+
+    def __str__(self):
+        return str(self.val)
+
     def __add__(self, other):
         return GF2(self.val ^ other.val)
 
@@ -135,7 +138,7 @@ class GF2_222:
         if fromint is None:
             self.coeff = [elem0, elem1]
         else:
-            assert fromint < 256, 'GF2_222 elemant bigger than 15.'
+            assert fromint < 256, 'GF2_222 elemant bigger than 255.'
             tmp = format(fromint, f'08b')
             x0 = fromint & 0xf
             x1 = fromint >> 4
@@ -168,12 +171,63 @@ class GF2_222:
         c0 = t1 + (self[0] * other[0])
         return GF2_22(c0, c1)
 
+class GF2_8:
+    """
+    Name:        GF2_8
+    A list of eight GF2 elements [x0, x1, x2,...x7], x1 and x0 are in GF2_22.
+    X in GF2_8 is represented as x0 + x1*A + x2*A^2,..., x7*A^7, [1, A, A^2, ..., A^7] is the polynomial basis.
+    x7 is the MSB. Polynomial basis representation.
+    """
+    def __init__(self, elem0: GF2 = GF2(), elem1: GF2 = GF2(), elem2: GF2 = GF2(), elem3: GF2 = GF2(), elem4: GF2 = GF2(), elem5: GF2 = GF2(), elem6: GF2 = GF2(), elem7: GF2 = GF2(), fromint: int = None):
+        if fromint is None:
+            self.coeff = [elem0, elem1, elem2, elem3, elem4, elem5, elem6, elem7]
+        else:
+            assert fromint < 256, 'GF2_8 elemant bigger than 255.'
+            x0 = fromint & 1
+            x1 = (fromint >> 1) & 1
+            x2 = (fromint >> 2) & 1
+            x3 = (fromint >> 3) & 1
+            x4 = (fromint >> 4) & 1
+            x5 = (fromint >> 5) & 1
+            x6 = (fromint >> 6) & 1
+            x7 = (fromint >> 7) & 1
+
+            self.coeff = [GF2(x0), GF2(x1), GF2(x2), GF2(x3), GF2(x4), GF2(x5), GF2(x6), GF2(x7)]
+
+    def __getitem__(self, index):
+        return self.coeff[index]
+
+    def __repr__(self):
+        return str(self.coeff)
+
+    def __mul__(self, other):
+        ### Multiplication on a polynomial basis.
+        ### Irreducible polynomial q(x) = x^8 + x^4 + x^3 + x + 1
+
+        t = [GF2()] * 15
+        for i in range(8):
+            for j in range(8):
+                t[j+i] = t[j+i] + (other[i] * self[j])
+
+        one = GF2(1)
+        for i in range(14, 7, -1):
+            if t[i].val == 1:
+                t[i-4] = t[i-4] + one
+                t[i-5] = t[i-5] + one
+                t[i-7] = t[i-7] + one
+                t[i-8] = t[i-8] + one
+        
+        return GF2_8(t[0], t[1], t[2], t[3], t[4], t[5], t[6], t[7])
+
+    def toInt(self):
+        return int("".join(list(reversed(list(map(str, self.coeff))))), 2)
+
 def matReduc(mat):
     reduced = []
     for row in mat:
         r = []
         for elem in row:
-            r.append(elem % 2)
+            r.append(int(elem % 2))
         reduced.append(r)
     return np.array(reduced)
 
@@ -219,7 +273,8 @@ def main():
         #print(f'{power17} == {a * a * a * a * a * a * a * a * a * a * a * a * a * a * a * a * a} ?')
         print(f'{a} * {a_inv} -> {a * a_inv}')
 
-    X = np.array([
+    ### Normal bases GF(2_222) to polynomial basis GF(2_8)
+    N2P = np.array([
         [0,0,0,1,0,0,1,0],
         [1,1,1,0,1,0,1,1],
         [1,1,1,0,1,1,0,1],
@@ -228,18 +283,27 @@ def main():
         [1,0,1,1,0,0,1,0],
         [0,0,1,0,0,0,1,0],
         [0,0,0,0,0,1,0,0]])
+    
+    P2N = matReduc(np.linalg.inv(N2P))
 
-    A = np.array([
-        [1,0,0,0,1,1,1,1],
-        [1,1,0,0,0,1,1,1],
-        [1,1,1,0,0,0,1,1],
-        [1,1,1,1,0,0,0,1],
+    Affine = np.array([
         [1,1,1,1,1,0,0,0],
         [0,1,1,1,1,1,0,0],
         [0,0,1,1,1,1,1,0],
-        [0,0,0,1,1,1,1,1]])
+        [0,0,0,1,1,1,1,1],
+        [1,0,0,0,1,1,1,1],
+        [1,1,0,0,0,1,1,1],
+        [1,1,1,0,0,0,1,1],
+        [1,1,1,1,0,0,0,1]])
         
-    print(matReduc(A@X))
-    print(matReduc(X@A))
+    N2PAffine = matReduc(Affine@N2P)
+
+    ### Base tranform vectors
+    y = GF2_8(fromint = 0xff)
+    z = GF2_8(fromint = 0x5c)
+    w = GF2_8(fromint = 0xbd)
+
+    print(hex((y*z*w).toInt()))
+
 if __name__ == "__main__":
     main()
