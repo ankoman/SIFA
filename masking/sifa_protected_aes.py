06254af33c718478334f06d1f8bd2289519dc865
ankoman <i08320.wctsonhb@gmail.com>
2024-05-21 19:15:43 +0900

    #

--- masking/sifa_protected_aes.py
+++ masking/sifa_protected_aes.py
@@ -1,8 +1,29 @@
 from aes_sbox import *
 import random
 
+def s_sc_4(a: GF2_22, b: GF2_22, c: GF2_22):
+    t0 = b + c
+    a = a + t0.sqr_scale()
+    return a, b, c
+
+def s_sc_2(a: GF2_2, b: GF2_2, c: GF2_2):
+    t0 = b + c
+    a = a + t0.sqr_scale()
+    return a, b, c
 
 def main():
+
+    ### Constraint generation
+    a0 = random.randint(0, 255)
+    a0 = a1
+    b0 = random.randint(0, 255)
+    b0 = b1
+    c0 = random.randint(0, 255)
+    c0 = c1
+    d0 = random.randint(0, 255)
+    d0 = d1
+
+    ### Input generation
     tv = 0x12
     mask = random.randint(0, 255)
     x0 = GF2_8(fromint = tv ^ mask)
@@ -10,7 +31,11 @@ def main():
 
     x0 = AES_map.to_GF2_222(x0)
     x1 = AES_map.to_GF2_222(x1)
-    print(x1)
+    x0_H, x0_L = x0[1], x0[0]
+    x1_H, x1_L = x1[1], x1[0]
+
+    a0, a1, b0, b1, c0, c1 = pTS_4(a0, a1, x0_H, x1_H, x0_L, x1_L)
+
 
 
 if __name__ == "__main__":
--- masking/toffoli.py
+++ masking/toffoli.py
@@ -11,7 +11,7 @@ def masked_toffoli(a0: bool, a1: bool, b0: bool, b1: bool, c0: bool, c1: bool):
     a1, b1, c0 = toffoli(a1, b1, c0)
     return a0, a1, b0, b1, c0, c1
 
-def pT_4(a: GF2_22, b: GF2_22, c: GF2_22) -> [GF2_22]:
+def pT_4(a: GF2_22, b: GF2_22, c: GF2_22) -> [GF2_22, GF2_22, GF2_22]:
     return a + (b * c), b, c
 
 def pTS_4(a0: GF2_22, a1: GF2_22, b0: GF2_22, b1: GF2_22, c0: GF2_22, c1: GF2_22):
@@ -21,18 +21,25 @@ def pTS_4(a0: GF2_22, a1: GF2_22, b0: GF2_22, b1: GF2_22, c0: GF2_22, c1: GF2_22
     a1, b1, c0 = pT_4(a1, b1, c0)
     return a0, a1, b0, b1, c0, c1
 
+def pT_2(a: GF2_2, b: GF2_2, c: GF2_2) -> [GF2_2, GF2_2, GF2_2]:
+    return a + (b * c), b, c
+
+def pTS_2(a0: GF2_2, a1: GF2_2, b0: GF2_2, b1: GF2_2, c0: GF2_2, c1: GF2_2):
+    a0, b0, c1 = pT_2(a0, b0, c1)
+    a0, b0, c0 = pT_2(a0, b0, c0)
+    a1, b1, c1 = pT_2(a1, b1, c1)
+    a1, b1, c0 = pT_2(a1, b1, c0)
+    return a0, a1, b0, b1, c0, c1
 
 def main():
-    ### Toffoli test
+    print('Toffoli test')
     for i in range(8):
         c = i & 1
         b = (i >> 1) & 1
         a = (i >> 2) & 1
         print(f'{a, b, c} -> {toffoli(a, b, c)}')
 
-    print()
-
-    ### Masked toffoli test
+    print('\nMasked toffoli test')
     for i in range(8):
         r = random.randint(0, 7)
         s = i ^ r
@@ -47,6 +54,12 @@ def main():
 
         print(f'{a0^a1, b0^b1, c0^c1}')
 
+    print('\npT_2 test')
+    for i in range(4*4*4):
+        c = GF2_2(fromint = i & 0x3)
+        b = GF2_2(fromint = (i >> 2) & 0x3)
+        a = GF2_2(fromint = (i >> 4) & 0x3)
+        print(f'{a, b, c} -> {pT_2(a, b, c)}')
 
 if __name__ == "__main__":
     main()
