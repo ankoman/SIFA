0fa0a6429ac7be618f77b771adea812489d2dc9b
ankoman <i08320.wctsonhb@gmail.com>
2024-05-23 16:10:16 +0900

    Small change

--- masking/sifa_protected_aes.py
+++ masking/sifa_protected_aes.py
@@ -13,25 +13,11 @@ def s_sc_2(a: GF2_2, b: GF2_2, c: GF2_2):
     a = a + t0.sqr_scale2()
     return a, b, c
 
-def main():
+def clone(x):
+    return deepcopy(x), x
 
-    ### Constraint generation
-    a0 = GF2_22(fromint = random.randint(0, 2**4 - 1))
-    a1 = deepcopy(a0)
-    b0 = GF2_2(fromint = random.randint(0, 2**2 - 1))
-    b1 = deepcopy(b0)
-    c0 = GF2_22(fromint = random.randint(0, 2**4 - 1))
-    c1 = deepcopy(c0)
-    d0 = GF2_222(fromint = random.randint(0, 2**8 - 1))
-    d1 = deepcopy(d0)
-
-    ### Input generation
-    tv = random.randint(0, 255)
-    X = GF2_8(fromint = tv)
-    X_222 = AES_map.to_GF2_222(X)
-    mask = random.randint(0, 255)
-    x0 = GF2_8(fromint =  mask)
-    x1 = X + x0
+def masked_sbox_v1(x0, x1, a0, a1, b0, b1, c0, c1, d0, d1):
+    ### Daemen's (8.a) construction with constraints.
 
     # Isomorphism
     x0 = AES_map.to_GF2_222(x0)
@@ -62,10 +48,48 @@ def main():
     const = GF2_8(fromint = 0x63)
     d0 = d0 + const
 
-    print(hex((d0+d1).toInt()))
-    print(hex(AES_map.Sbox[tv]))
+    return x0, x1, a0, a1, b0, b1, c0, c1, d0, d1
 
+def masked_sbox_v2(x0, x1, a0, b0, c0, d0):
+    ### Daemen's (8.b) construction with cloning.
 
+    # Cloning
+    a1, a0 = clone(a0)
+    b1, b0 = clone(b0)
+    c1, c0 = clone(c0)
+    d1, d0 = clone(d0)
+
+    e0, e1, f0, f1, g0, g1, h0, h1, y0, y1 = masked_sbox_v1(x0, x1, a0, a1, b0, b1, c0, c1, d0, d1)
+
+    return y0, y1, f0, g0, h0, e0
+
+def main():
+
+    ### Constraint generation
+    a0 = GF2_22(fromint = random.randint(0, 2**4 - 1))
+    a1 = deepcopy(a0)
+    b0 = GF2_2(fromint = random.randint(0, 2**2 - 1))
+    b1 = deepcopy(b0)
+    c0 = GF2_22(fromint = random.randint(0, 2**4 - 1))
+    c1 = deepcopy(c0)
+    d0 = GF2_222(fromint = random.randint(0, 2**8 - 1))
+    d1 = deepcopy(d0)
+
+    ### Input generation
+    tv = random.randint(0, 255)
+    X = GF2_8(fromint = tv)
+    X_222 = AES_map.to_GF2_222(X)
+    mask = random.randint(0, 255)
+    x0 = GF2_8(fromint =  mask)
+    x1 = X + x0
+
+    e0, e1, f0, f1, g0, g1, h0, h1, y0, y1 = masked_sbox_v1(x0, x1, a0, a1, b0, b1, c0, c1, d0, d1)
+    print(f'v1:  {hex((y0+y1).toInt())}')
+
+    y0, y1, f0, g0, h0, e0 = masked_sbox_v2(x0, x1, a0, b0, c0, d0)
+    print(f'v2:  {hex((y0+y1).toInt())}')
+
+    print(f'ANS: {hex(AES_map.Sbox[tv])}')
 
 if __name__ == "__main__":
     main()
