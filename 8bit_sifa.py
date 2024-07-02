583ed953db52a72fc74069e90f9155a64014d0d9
ankoman <i08320.wctsonhb@gmail.com>
2024-07-02 18:59:54 +0900

    Add multi_bits analysis

--- 8bit_sifa.py
+++ 8bit_sifa.py
@@ -1,15 +1,17 @@
 from aes import Sbox, InvSbox, AES, text2column, column2text, mix_single_column, inv_mix_single_column, sub_bytes_column, inv_sub_bytes_column
 from tqdm import tqdm
-import random
-import math
+import random, math, itertools
 random.seed(20)
 
 dist_bin = [1, 8, 28, 56, 70, 56, 28, 8, 1]
 
-ANALYSIS_MODEL = "8bits_HW" ### ['n'bits_HW or 'n'bits]
+ANALYSIS_MODEL = "5bits" ### ['n'bits_HW or 'n'bits]
 ATTACK_LOCATION = "SB_in" ### ['SB_in' or "MC_in"]
+TARGET_BITS = None #(0,1,2,3,4) ### A tuple of 0-7 or None
 print(ANALYSIS_MODEL)
 n_bits = int(ANALYSIS_MODEL[0])
+if not TARGET_BITS is None:
+    assert n_bits == len(TARGET_BITS), "The number of model bits and target bits differ"
 power_two = 2**n_bits
 n_ave_key = 64
 
@@ -26,7 +28,7 @@ def fault_injection(x, correct_key, fault_injected = 0):
         fault_byte = x
 
     if fault_injected:
-        fault_byte = (fault_byte & 0xf0) | (fault_byte & random.randint(0,0xf))
+        fault_byte = (fault_byte & 0xe0) | (fault_byte & random.randint(0,0x1f))
         # fault_byte &= random.randint(0,0xff)
         # fault_byte &= 0x1
 
@@ -52,12 +54,17 @@ def uniform_dist(x):
     else:
         return 1/power_two
 
-def leak_f(x):
+def leak_f(x, _target_bits):
     if "HW" in ANALYSIS_MODEL:
         f = HW
     else:
         f = identity
-    return f(x & (power_two - 1))
+    
+    y = 0
+    for bit_pos in _target_bits:
+        y <<= 1
+        y |= f((x & (2**bit_pos)) >> bit_pos)
+    return y
 
 def get_SEI(key_hyp, list_ineffective, n_ineffective):
     if "HW" in ANALYSIS_MODEL:
@@ -66,7 +73,7 @@ def get_SEI(key_hyp, list_ineffective, n_ineffective):
         n_leak_val = power_two
         
     ### Init dictionary
-    list_freq = [0]*n_leak_val
+    list_freq = [[0]*n_leak_val for i in range(math.comb(8, n_bits))]
 
     for ctxt in list_ineffective:
         if ATTACK_LOCATION == "MC_in":
@@ -79,8 +86,14 @@ def get_SEI(key_hyp, list_ineffective, n_ineffective):
         elif ATTACK_LOCATION == "SB_in":
             t0 = ctxt ^ key_hyp
             t1 = InvSbox[t0]
-        leak = leak_f(t1)
-        list_freq[leak] += 1
+        if TARGET_BITS is None:
+            for i, target in enumerate(itertools.combinations([0,1,2,3,4,5,6,7], n_bits)):
+                leak = leak_f(t1, target)
+                list_freq[i][leak] += 1
+        else:
+            leak = leak_f(t1, TARGET_BITS)
+            list_freq[0][leak] += 1
+
     ### Calc SEI
     # sei = 0
     # for j in range(n_leak_val):
@@ -89,18 +102,22 @@ def get_SEI(key_hyp, list_ineffective, n_ineffective):
 
     ### Calc CHI
     chi = 0
-    for j in range(n_leak_val):
-        chi += ((list_freq[j]/n_ineffective - uniform_dist(j))**2) / uniform_dist(j)
-    #     print(list_freq[j]/n_ineffective)
-    # print(chi)
-    # print(n_ineffective)
-    # input()
+    if TARGET_BITS is None:
+        for i in range(math.comb(8, n_bits)):
+            t_chi = 0
+            for j in range(n_leak_val):
+                t_chi += ((list_freq[i][j]/n_ineffective - uniform_dist(j))**2) / uniform_dist(j)
+            chi = max(chi, t_chi)
+    else:
+        for j in range(n_leak_val):
+            chi += ((list_freq[0][j]/n_ineffective - uniform_dist(j))**2) / uniform_dist(j)
+
     return chi*n_ineffective
 
 
 def main():
 
-    for n_enc in range(100, 2010, 100):
+    for n_enc in range(10, 310, 10):
         ave_rank = 0
         ave_sei_correct = 0
         ave_sei_wrong_max = 0
@@ -126,6 +143,7 @@ def main():
             n_ineffective = len(list_ineffective)
             # print(f'{n_ineffective} ineffective faults out of {n_enc} fault injection ({n_ineffective/n_enc*100} %)')
 
+            sei_correct, sei_wrong_max, sei_wrong_mu = 0, 0, 0
             if n_ineffective > 0:
                 for key_hyp in range(256):
                     dict_SEI[hex(key_hyp)] = get_SEI(key_hyp, list_ineffective, n_ineffective)
@@ -156,9 +174,9 @@ def main():
         ave_sei_wrong_max /= n_ave_key
         ave_sei_wrong_mu /= n_ave_key
         ave_n_ineff /= n_ave_key
-        print(f"\n #{n_enc} Ave. correct key rank = {ave_rank:.1f}, Ave. sei_r = {ave_sei_correct:.1f}, Ave. sei_w_max = {ave_sei_wrong_max:.1f}, "
-                f"Ave, sei_w_mu = {ave_sei_wrong_mu:.1f}, Ave. n_ineff = {ave_n_ineff:.1f}, n_rank_1 = {n_rank_1}")
-        # print(f"{ave_sei_correct},{ave_sei_wrong_max},{ave_sei_wrong_mu}")
+        # print(f"#{n_enc} Ave. correct key rank = {ave_rank:.1f}, Ave. sei_r = {ave_sei_correct:.1f}, Ave. sei_w_max = {ave_sei_wrong_max:.1f}, "
+                # f"Ave, sei_w_mu = {ave_sei_wrong_mu:.1f}, Ave. n_ineff = {ave_n_ineff:.1f}, n_rank_1 = {n_rank_1}")
+        print(f"{ave_sei_correct},{ave_sei_wrong_max},{ave_sei_wrong_mu}")
 
 if __name__ == '__main__':
     main()
